8fc03bd50c9a818e0dac0883103865f0f5c9800e
Steffen <steffen@MacBook-Pro-von-Steffen.local>
2023-05-09 18:20:41 +0200

    adding navigation, all needed data, fonts

--- src/pages/[year]/[month].tsx
+++ src/pages/[year]/[month].tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 import {
   TOrdersResponse,
   TOrdersResponseObject,
@@ -23,16 +24,42 @@ export default function Home({
   const ordersByDate = [...monthOrders]?.sort(
     (a, b) => new Date(a.date).getDate() - new Date(b.date).getDate()
   );
+
+  const ordersByAmount = monthOrders.sort((a, b) => b.amount - a.amount);
   const total = ordersByDate.reduce((acc, curr) => acc + curr.amount, 0);
+
+  const monthNumber = parseInt(params.month);
   const yearNumber = parseInt(params.year);
+  const previousYear = monthNumber === 1 ? yearNumber - 1 : yearNumber;
+  const previousMonth = monthNumber === 1 ? 12 : monthNumber - 1;
+  const nextYear = monthNumber === 12 ? yearNumber + 1 : yearNumber;
+  const nextMonth = monthNumber === 12 ? 1 : monthNumber + 1;
   const currentMonthString = monthTarget.id;
   return (
     <>
       <main className={styles.main}>
+        <Link href={`/${previousYear}/${previousMonth}`}>Prev</Link>
+        <Link href={`/${nextYear}/${nextMonth}`}>Next</Link>
         <div>
           {currentMonthString} {yearNumber}
         </div>
         <div>Max Target: {maxTarget}</div>
+        {ordersByDate.map((orderByDate) => {
+          const dateObj = new Date(orderByDate.date);
+
+          const month = dateObj.getMonth() + 1;
+          const day = dateObj.getDate();
+          const year = dateObj.getFullYear();
+          return (
+            <div key={crypto.randomUUID()}>
+              {day}.{month}.{year}{" "}
+              {orderByDate.amount.toLocaleString("de-DE", {
+                style: "currency",
+                currency: "EUR",
+              })}
+            </div>
+          );
+        })}
         <div>
           Total{" "}
           {total.toLocaleString("de-DE", {
@@ -40,6 +67,21 @@ export default function Home({
             currency: "EUR",
           })}
         </div>
+        {ordersByAmount.slice(0, 5).map((orderByAmount) => {
+          const dateObj = new Date(orderByAmount.date);
+          const month = dateObj.getMonth() + 1;
+          const day = dateObj.getDate();
+          const year = dateObj.getFullYear();
+          return (
+            <div key={crypto.randomUUID()}>
+              {day}.{month}.{year}{" "}
+              {orderByAmount.amount.toLocaleString("de-DE", {
+                style: "currency",
+                currency: "EUR",
+              })}
+            </div>
+          );
+        })}
         <div>
           Monthly Target:{" "}
           {monthTarget.amount.toLocaleString("de-DE", {
@@ -114,5 +156,6 @@ export const getStaticProps = async ({ params }: any) => {
       monthTarget,
       maxTarget: targetsByAmount[0].amount,
     },
+    revalidate: 30,
   };
 };
